dc26967ae68c279e97445ab994bd1023903edaf2
Rio Adrian <rioadriansky02@gmail.com>
2025-02-04 19:29:50 +0700

    edit backdrop-filter modal-tambah

--- src/components/modal-tambah.js
+++ src/components/modal-tambah.js
@@ -23,8 +23,8 @@ class ModalTambah extends HTMLElement{
                 left: 0;
                 width: 100%;
                 height: 100vh;
-                background: rgba(0, 0, 0, 0.1); 
-                backdrop-filter: blur(2px); 
+                background: rgba(0, 0, 0, 0.7); 
+                backdrop-filter: blur(3px); 
                 justify-content: center;
                 align-items: center;
             }
